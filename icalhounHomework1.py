bdf89846340a84912de5a4afc79b57cb5f273c84
ICalhoun <icalhoun@student.bridgew.edu>
2021-04-27 16:32:18 -0400

    fixed get_price_with_tax to account for massachusetts tax on clothes over 175

--- icalhounHomework1.py
+++ icalhounHomework1.py
@@ -10,7 +10,7 @@ class PurchasedItem(object):
 
 def __get_tax_rate(state, items):
     tax_rates = {
-        "ma": {"clothes": 0.0625, "wic eligible food": 0.0, "everything else": 0.0625},
+        "ma": {"clothes": 0.0, "wic eligible food": 0.0, "everything else": 0.0625},
         "nh": {"clothes": 0.0, "wic eligible food": 0.0, "everything else": 0.0},
         "me": {"clothes": 0.055, "wic eligible food": 0.0, "everything else": 0.055},
         "massachusetts": {
@@ -25,12 +25,18 @@ def __get_tax_rate(state, items):
         },
         "maine": {"clothes": 0.055, "wic eligible food": 0.0, "everything else": 0.055},
     }
-    return tax_rates[state.lower()][items.type.lower()]
+    if state.lower() == 'ma':
+        return 0.0625
+    else:
+        return tax_rates[state.lower()][items.type.lower()]
 
 
 def __get_price_with_tax(state, item):
     tax_rate = __get_tax_rate(state, item)
-    taxed_price = (tax_rate * item.price) + item.price
+    if state.lower() == 'ma' and item.type.lower() == 'clothes' and item.price > 175:
+        taxed_price = (tax_rate * (item.price - 175)) + item.price
+    else:
+        taxed_price = (tax_rate * item.price) + item.price
     return taxed_price
 
 
@@ -38,15 +44,13 @@ def calculate_total(state, customer_items):
     total = 0
     for items in customer_items:
         if items.price > 0.0:
-            total = total + __get_price_with_tax(state, items)
+                total = total + __get_price_with_tax(state, items)
     return round(total, 2)
 
 
 if __name__ == "__main__":
     purchases = [
         PurchasedItem("test", 199.00, "clothes"),
-        PurchasedItem("test2", 10.00, "everything else"),
-        PurchasedItem("test3", 10.00, "everything else"),
     ]
 
     print("The total is: $" + str(calculate_total("MA", purchases)))
